4c40edf1a5aaa0817c9ad0854e29aea70eb2a23e
kurong00 <kurong00@gmail.com>
2017-10-13 18:32:04 +0800

    init commit

---
+++ Assets/Scripts/UIItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIItem : MonoBehaviour {
+
+    Image itemImage;
+    Text itemName;
+    Text itemCount;
+	void Start () {
+        itemImage = GetComponent<Image>();
+        itemName = transform.GetChild(0).GetComponent<Text>();
+        itemCount = transform.GetChild(1).GetComponent<Text>();
+    }
+	
+	
+	void Update () {
+		
+	}
+
+    void SetUIItemInformation()
+    {
+        
+    }
+}
